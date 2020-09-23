d6a845c7f0f59a297662e466622573ba62f7a604
hiitoos <adrianhitos@gmail.com>
2020-09-23 08:50:09 +0200

    solved merged file with bad status

--- src/main/java/com/hotel/java/configuration/UiSecurityConfiguration.java
+++ src/main/java/com/hotel/java/configuration/UiSecurityConfiguration.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.hotel.java.configuration;
 
 
@@ -127,54 +126,3 @@ class PasswordEnconderTest implements PasswordEncoder {
         return charSequence.toString().equals(s);
     }
 }
-=======
-package com.hotel.java.configuration;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class UiSecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests()
-			.antMatchers("/reservas/showall").permitAll()
-			.anyRequest().authenticated()
-		.and()
-			.formLogin()
-			.loginPage("/login")
-			.permitAll()
-		.and()
-			.logout()
-			.permitAll();
-
-	}
-
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-
-		UserDetails user = User.withDefaultPasswordEncoder()
-			.username("user")
-			.password("secret")
-			.roles("USER")
-			.build();
-
-		return new InMemoryUserDetailsManager(user);
-
-	}
-
-}
->>>>>>> 6d64d62217f09efbd6db44783daa9616f9dd10ee
