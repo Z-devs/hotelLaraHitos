a162a4dfc4700e99be71cf26adbf18ba39d1597d
hiitoos <adrianhitos@gmail.com>
2020-10-05 20:54:57 +0200

    some stuff done

--- src/main/resources/static/js/habitaciones.js
+++
@@ -1,9 +0,0 @@
-// INSERT JS HERE
-function calculaPrecio(checkIn, checkOut, precioHab) {
-    var fecha1 = moment(checkIn);
-    var fecha2 = moment(checkOut);
-
-    var precio = fecha2.diff(fecha1, 'days') * precioHab;
-    return precio;
-}
-
